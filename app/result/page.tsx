ec42efd9b59a68eef4e45096e54e76849315a244
dulkifal <dulkifilibacker@gmail.com>
2023-05-09 14:35:48 +0530

    result postion top

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -3,47 +3,43 @@ import styles from '../page.module.css'
 export default function Result() {
     return (
         <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-           Admission Test Result &nbsp;
-            <code className={styles.code}>2023-24</code>
-          </p>
-          <div>
-           
-          
-           
-          </div>
-        </div>
-         
-  
-        <div className={styles.center}>
-         {/* set a table Reg,Name, Result  horizotally*/}
+            <div className={styles.description}>
+                <p>
+                    Admission Test Result &nbsp;
+                    <code className={styles.code}>2023-24</code>
+                </p>
+                
+            </div>
+
+
+            <div className={styles.result}>
             
-            <table className={styles.table}>
-                <tr>
-                    <th>Reg.No</th>
-                    <th> Name</th>
-                    <th>Result</th>
+
+                <table className={styles.table}>
+                    <tr>
+                        <th>Reg.No</th>
+                        <th> Name</th>
+                        <th>Result</th>
                     </tr>
-                <tr>
-                    <td>AT0001</td>
-                    <td>Abdul Haseeb Rasz </td>
-                    <td>Pass</td>
-                </tr>
+                    <tr>
+                        <td>AT0001</td>
+                        <td>Abdul Haseeb Rasz </td>
+                        <td>Pass</td>
+                        
+                    </tr>
 
 
-            </table>
-           
+                </table>
 
-         
-         
-        </div>
-        
 
-        <div className={styles.grid}>
-            <div className={styles.card}>
-                </div>
-        </div>
+
+
+            </div>
+
+
+            <div className={styles.grid}>
+                
+            </div>
         </main>
 
     )
